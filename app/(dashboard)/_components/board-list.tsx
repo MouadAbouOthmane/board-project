3de8347d9286d129ad65dc2fa37919279f69c9b2
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2024-02-19 13:40:12 +0100

    finish card actions

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -7,6 +7,7 @@ import { EmptyBoards } from "./empty-boards";
 import { EmptyFavorites } from "./empty-favorites";
 import { EmptySearch } from "./empty-search";
 import { BoardCard } from "./board-card";
+import { NewBoardButton } from "./new-board-button";
 
 interface BoardListProps {
   orgId: string;
@@ -19,7 +20,26 @@ interface BoardListProps {
 export const BoardList = ({ orgId, query }: BoardListProps) => {
   const data = useQuery(api.boards.get, { orgId });
 
-  if (data === undefined) return <div> Loading ...</div>;
+  if (data === undefined)
+    return (
+      <div>
+        {" "}
+        <h2 className="text-3xl">
+          {query.favorites ? "Favorite boards" : "Team boards"}
+        </h2>
+        <div
+          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:gr-col-4
+      xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10"
+        >
+          <NewBoardButton orgId={orgId} disabled />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+        </div>
+      </div>
+    );
 
   if (!data?.length && query.search) return <EmptySearch />;
 
@@ -37,6 +57,8 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
         className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:gr-col-4
       xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10"
       >
+        <NewBoardButton orgId={orgId} />
+
         {data?.map((board) => (
           <BoardCard
             key={board._id}
--- convex/board.ts
+++ convex/board.ts
@@ -1,6 +1,7 @@
 import { v } from "convex/values";
 
 import { mutation } from "./_generated/server";
+import { title } from "process";
 
 const images = [
   "/placeholders/1.svg",
@@ -37,3 +38,42 @@ export const create = mutation({
     return board;
   },
 });
+
+export const remove = mutation({
+  args: { id: v.id("boards") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) throw new Error("Unauthorized");
+
+    // TODO: later check to delete favorite relation as well
+    await ctx.db.delete(args.id);
+  },
+});
+
+
+export const update = mutation({
+  args: { id: v.id("boards"), title: v.string() },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity()
+
+    if (!identity)
+      throw new Error("Unauthorized")
+
+    const title = args.title.trim()
+
+    if (!title) {
+      throw new Error("Title is required")
+    }
+
+    if (title.length > 60) {
+      throw new Error("Title cannot be longer than 60 characters")
+    }
+
+    const board = await ctx.db.patch(args.id, {
+      title: args.title
+    })
+
+    return board
+  }
+})
