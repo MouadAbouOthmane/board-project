06149ac397b14f5459f37cec521539440f865bcb
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2024-02-23 17:10:47 +0100

    end 23-02-2024

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,22 +1,15 @@
-import { Camera } from "@/types/canvas"
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { Camera, Color } from "@/types/canvas";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-const COLORS = [
-  '#DC2626',
-  '#d97706',
-  '#059669',
-  '#7c3aed',
-  '#db2777'
-]
+const COLORS = ["#DC2626", "#d97706", "#059669", "#7c3aed", "#db2777"];
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-
 export function connectionIdToColor(connectionId: number): string {
-  return COLORS[connectionId % COLORS.length]
+  return COLORS[connectionId % COLORS.length];
 }
 
 export function pointerEventToCanvasPoint(
@@ -24,7 +17,13 @@ export function pointerEventToCanvasPoint(
   camera: Camera
 ) {
   return {
-    x: Math.round(e.clientX) - camera.x ,
-    y: Math.round(e.clientY) - camera.y 
-  }
+    x: Math.round(e.clientX) - camera.x,
+    y: Math.round(e.clientY) - camera.y,
+  };
+}
+
+export function colorToCss(color: Color) {
+  return `#${color.r.toString(16).padStart(2, "0")}${color.g
+    .toString(16)
+    .padStart(2, "0")}${color.b.toString(16).padStart(2, "0")}`;
 }
