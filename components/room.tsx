58e0179751272f801663f74bcd86fd2ef237a000
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2024-02-23 16:06:15 +0100

    insert Layer

--- components/room.tsx
+++ components/room.tsx
@@ -1,26 +1,32 @@
-"use client"
+"use client";
 
-import { ReactNode } from "react"
-import { ClientSideSuspense } from "@liveblocks/react" 
+import { ReactNode } from "react";
+import { ClientSideSuspense } from "@liveblocks/react";
 
-import { RoomProvider } from "@/liveblocks.config"
+import { RoomProvider } from "@/liveblocks.config";
+import { LiveMap, LiveObject, LiveList } from "@liveblocks/client";
+
+import { Layer } from "@/types/canvas";
 
 interface RoomProps {
-    children: ReactNode
-    roomId: string
-    fallback: NonNullable<ReactNode> | null
+  children: ReactNode;
+  roomId: string;
+  fallback: NonNullable<ReactNode> | null;
 }
 
-export const Room = ({
-    children,
-    roomId,
-    fallback
- }: RoomProps) => {
-    return (
-        <RoomProvider id={roomId} initialPresence={{ cursor: null}}>
-            <ClientSideSuspense fallback={fallback}>
-                {() => children}
-            </ClientSideSuspense>
-        </RoomProvider>
-    )
-} 
+export const Room = ({ children, roomId, fallback }: RoomProps) => {
+  return (
+    <RoomProvider
+      id={roomId}
+      initialPresence={{ cursor: null, selection: [] }}
+      initialStorage={{
+        layers: new LiveMap<string, LiveObject<Layer>>(),
+        layerIds: new LiveList(),
+      }}
+    >
+      <ClientSideSuspense fallback={fallback}>
+        {() => children}
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
