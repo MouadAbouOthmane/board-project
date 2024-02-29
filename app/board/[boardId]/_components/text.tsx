56941cab1721701e07885dbca3d19e6f9e206fda
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2024-02-29 18:03:20 +0100

    end day 29-02-2024

---
+++ app/board/[boardId]/_components/text.tsx
@@ -0,0 +1,41 @@
+import { Kalam } from "next/font/google";
+import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
+
+import { cn, colorToCss } from "@/lib/utils";
+import { TextLayer } from "@/types/canvas";
+
+const font = Kalam({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+interface TextProps {
+  id: string;
+  layer: TextLayer;
+  onPointerDown: (e: React.PointerEvent, id: string) => void;
+  selectionColor?: string;
+}
+
+export const Text = ({
+  layer,
+  onPointerDown,
+  id,
+  selectionColor,
+}: TextProps) => {
+  const { x, y, width, height, fill, value } = layer;
+
+  return (
+    <foreignObject
+        x={x}
+        y={y}
+        width={width}
+        height={height}
+        onPointerDown={(e) => onPointerDown(e, id)}
+        style={{
+            outline: selectionColor ? `1px solid ${selectionColor}` : "none"
+        }}
+    >
+      <ContentEditable html={"Text"} onChange={() => {}} />
+    </foreignObject>
+  );
+};
