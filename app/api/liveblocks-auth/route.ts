788f91c2f4f4275071dbf9160e9d5cfe02c7efac
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2024-03-01 14:55:28 +0100

    deployment

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -8,8 +8,7 @@ import { api } from "@/convex/_generated/api";
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 const liveblocks = new Liveblocks({
-  secret:
-    "sk_dev_PQ8e6SCF9QV9R0t6SFSibZe_gx5ZOMwiudiyLl0yZB1Ws248kNrRWnS0Nh8_RcUY",
+  secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
 
 export async function POST(request: Request) {
